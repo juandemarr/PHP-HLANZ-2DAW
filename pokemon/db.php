7517eed80b69388afeff9c0f8e350896ae3f15d3
juandemarr <cloud15jc@gmail.com>
2022-03-15 22:36:06 +0100

    removing ee

--- pokemon/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Connection{
-    public static function connect(){
-        $db = new mysqli("localhost", "root", "", "examenJuande");
-        $db->query("set names 'utf8'");
-        return $db;
-    }
-}
-?>
